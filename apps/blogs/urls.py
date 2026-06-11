8e46785aead0a66e722c4de45e82101ba7be10f9
kennedyngigi4 <kennedyngigi4@gmail.com>
2026-06-11 15:54:34 +0300

    Blog details fixing

--- apps/blogs/urls.py
+++ apps/blogs/urls.py
@@ -3,6 +3,6 @@ from apps.blogs.views import AllBlogsView, BlogRetrieveView
 
 urlpatterns = [
     path( "all/", AllBlogsView.as_view(), name="all"),
-    path( "blogs/<slug:slug>/", BlogRetrieveView.as_view(), name="blog-detail", )
+    path( "blog_details/<slug:slug>/", BlogRetrieveView.as_view(), name="blog_details", )
 ]
 
